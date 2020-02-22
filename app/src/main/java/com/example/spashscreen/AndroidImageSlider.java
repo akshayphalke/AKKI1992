13dff30676e5f837a46b9e3bedc94dbaa4c8baa6
Akshay <akshay9869@gmail.com>
2020-02-22 12:23:35 +0530

    Spashscreen

---
+++ app/src/main/java/com/example/spashscreen/AndroidImageSlider.java
@@ -0,0 +1,24 @@
+package com.example.spashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AndroidImageSlider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_image_slider);
+
+
+
+
+
+
+
+
+
+
+    }
+}
